25b3938e9c7fe97245a4b8494079681975530ee0
Nolan266 <juro@gmail.com>
2026-09-08 07:21:11 +0700

    Soal 2 Validasi Nama

---
+++ app/variable/ValidasiNama.jsx
@@ -0,0 +1,21 @@
+// Soal 2: Validasi Nama Pengguna (Username Trim & Length)
+
+export default function ValidasiNamaPenggunaBaru() {
+  const rawUsername = " marcella_admin ";
+  const countUsernameBeforeFiltered = rawUsername.length;
+  const usernameFiltered = rawUsername.trim();
+  const countUsernameAfrerFiltered = usernameFiltered.length;
+
+  return (
+    <div>
+      <h2>Raw Username / Username Mentah</h2>
+      <p>
+        \[{rawUsername}\] dengan jumlah huruf {countUsernameBeforeFiltered}
+      </p>
+      <h2>Username Filtered / Username Setelah Terfilter</h2>
+      <p>
+        \[{usernameFiltered}\] dengan jumlah huruf {countUsernameAfrerFiltered}
+      </p>
+    </div>
+  );
+}
